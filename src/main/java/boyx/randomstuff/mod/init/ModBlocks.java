69f27374f75cfa0ca6dda2b558eb0ae2d63b67d3
boyx13 <flippi1313@gmail.com>
2019-12-15 01:39:30 +0100

    Added ModBlocks

---
+++ src/main/java/boyx/randomstuff/mod/init/ModBlocks.java
@@ -0,0 +1,13 @@
+package boyx.randomstuff.mod.init;
+
+import boyx.randomstuff.mod.util.Reference;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@GameRegistry.ObjectHolder(Reference.MODID)
+public class ModBlocks {
+	
+	public static void init() {
+		
+	}
+
+}

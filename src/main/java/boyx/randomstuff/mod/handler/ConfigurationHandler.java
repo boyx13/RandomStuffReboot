db78bf58fc0c96e0c55e228a21b8b4b4750508ef
boyx13 <flippi1313@gmail.com>
2019-12-15 22:57:25 +0100

    ConfigHandler finished

--- src/main/java/boyx/randomstuff/mod/handler/ConfigurationHandler.java
+++ src/main/java/boyx/randomstuff/mod/handler/ConfigurationHandler.java
@@ -2,6 +2,7 @@ package boyx.randomstuff.mod.handler;
 
 import java.io.File;
 
+import boyx.randomstuff.mod.util.Log;
 import net.minecraftforge.common.config.Configuration;
 
 public class ConfigurationHandler {
@@ -9,13 +10,16 @@ public class ConfigurationHandler {
 	public static void init(File configFile) {
 		
 		Configuration configuration = new Configuration(configFile);
+		boolean configValue = false;
 		
 		try {
 			configuration.load();
+			configValue = configuration.get(Configuration.CATEGORY_GENERAL, "configValue", true, "This is an example").getBoolean(true);
 			}catch(Exception e) {
-				
+				Log.error("Config File Error");
 			}finally {
 				configuration.save();
+				Log.info("Config Loaded!");
 			}
 	}
 
---
+++ src/main/java/boyx/randomstuff/mod/item/ItemClass.java
@@ -0,0 +1,8 @@
+package boyx.randomstuff.mod.item;
+
+import net.minecraft.item.Item;
+
+public class ItemClass extends Item{
+	
+	
+}
